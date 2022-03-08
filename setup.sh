ae8e32870c68084285f8eccdc4a541a7788232c5
Taha Khabouss <tkhabous@e1r11p2.1337.ma>
2022-03-08 04:50:10 +0100

    update script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ export PATH=$HOME/.orthodox:$PATH
 echo "export PATH=$HOME/.orthodox:$PATH" >> $HOME/.zshrc
 echo "export PATH=$HOME/.orthodox:$PATH" >> $HOME/.bashrc
 make fclean
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${green}ALL DONE! ENJOY :D - github.com/khabouss/orthodox${reset}"
